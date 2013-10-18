884b548a7146ca9c36a1963ae1e5aafe3bebd8f5
Taras Glek <taras@mozilla.com>
2013-10-17 17:23:27 -0700

    checkpoint

--- spotcosts.js
+++ spotcosts.js
@@ -2,30 +2,43 @@ aws = require('aws-lib');
 fs = require('fs');
 async = require('async');
 
-var keys = fs.readFileSync(process.argv[2]).toString().split("\r\n");
+var keys = fs.readFileSync(process.argv[2]).toString().split("\n");
 
-const AWS_ACCESS_KEY_ID = keys[0];
-const AWS_SECRET_ACCESS_KEY = keys[1];
+const AWS_ACCESS_KEY_ID = keys[0].trim();
+const AWS_SECRET_ACCESS_KEY = keys[1].trim();
 
-getData = function(region, callback) {
+getSpotPrices = function(region, callback) {
   var ec2;
-  console.log('starting ' + region);
   ec2 = aws.createEC2Client(AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY, {
-    host: 'ec2.' + region + '.amazonaws.com',
-    version: '2012-05-01'
+    host: 'ec2.' + region + '.amazonaws.com'
+ 
   });
   return ec2.call("DescribeSpotPriceHistory", {
     ProductDescription: 'Linux/UNIX'
   }, function(err, data) {
+//jq '.item[] as $o| [$o.instanceType, $o.spotPrice, $o.timestamp, $o.availabilityZone]' -c  < ret.json
 //    return async.map(data.spotPriceHistorySet.item, buildItem, function(err, results) {
-    console.log('finished ' + region);
-    console.log(JSON.stringify(data.spotPriceHistorySet));
+    //console.log('finished ' + region);
+    console.log(JSON.stringify(data.spotPriceHistorySet.item));
   //  });
   });
 };
 
-async.map(['us-east-1', 'us-west-1', 'us-west-2'], getData, function(err, results) {
-  var code;
-  console.log('finished ' + results);
-  return process.exit(code = 0);
-});
+listInstances = function(region, callback) {
+  var ec2;
+  ec2 = aws.createEC2Client(AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY, {
+    host: 'ec2.' + region + '.amazonaws.com'
+ 
+  });
+// jq '.[].instancesSet.item as $i | [$i.instanceType,$i.placement,$i.instanceState,$i.instanceLifecycle]'
+  return ec2.call("DescribeInstances", {
+  }, function(err, data) {
+//    return async.map(data.spotPriceHistorySet.item, buildItem, function(err, results) {
+    //console.log('finished ' + region);
+    console.log(JSON.stringify(data.reservationSet.item));
+  //  });
+  });
+};
+
+listInstances('us-west-2');
+
