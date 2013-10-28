a8605cb6298c55589335cc6d72b372fd37ad5527
Taras Glek <taras@mozilla.com>
2013-10-28 10:25:33 -0700

    added pricetop

--- spotlog.js
+++ spotlog.js
@@ -1,5 +1,5 @@
-var listRef = new Firebase('https://ec2-dev-activity.firebaseio.com/log');
-var awsPrices = new Firebase('https://ec2-dev-activity.firebaseio.com/ondemand/us-west-2');
+const FIREBASE = 'https://ec2-dev-activity.firebaseio.com/'
+var awsPrices = new Firebase(FIREBASE + 'ondemand/us-west-2');
 
 function age(timestamp) {
   var diff_s = (new Date() - timestamp)/1000
@@ -15,17 +15,16 @@ function age(timestamp) {
   return Math.round(diff_d) + " days"
 }
 
-listRef.on('child_added', function(snapshot) {
-  var l = snapshot.val();
+function output(l, show_age, price_filter) {
   /*if (!l.timestamp) {
    snapshot.ref().remove();
    return;
    }*/
   var str =  l.data.instanceId + "("+l.data.instanceType+") " + l.state + " " + l.data.keyName;
-
-  var node = $("<div><i>"+age(l.timestamp)+"</i>: "
-           + str + "</div>");
-  if (l.state.indexOf("->terminated")!=-1) {
+  var ageStr = show_age ? "<i>" + age(l.timestamp) + "</i>: " : "";
+  var node = $("<div>" + ageStr
+               + str + "</div>");
+  if (!price_filter || l.state.indexOf(price_filter)!=-1) {
     var diff = l.timestamp - new Date(l.data.launchTime)
     var type = l.data.instanceType
     awsPrices.child(type.replace('.', '_')).on('value', function(snapshot) {
@@ -34,7 +33,35 @@ listRef.on('child_added', function(snapshot) {
       node.append($("<span><b> $"+(hours * perHour)+"</b> ("+hours+" hours)</span>"))
     })
   }
-//  console.log(l)
+  //  console.log(l)
   document.title = l.state + " " + l.data.instanceId;
   $( ".container" ).prepend( node );
-});
+}
+
+function pricelog () {
+  var listRef = new Firebase(FIREBASE + 'log');
+  listRef.on('child_added', function(snapshot) {
+    output(snapshot.val(), true, "->terminated");
+  });
+}
+
+function pricetop () {
+  $( ".container" ).empty();
+  var firebaseOld = new Firebase(FIREBASE + "old");
+  firebaseOld.on('value', function (snapshot) {
+    var old = snapshot.val();
+    var running = []
+    for (var instanceId in old) {
+      var o = old[instanceId];
+      if (o.instanceState.name != "running")
+        continue;
+      running.push(o);
+      o.launchTime = new Date(o.launchTime)
+    }
+    running.sort(function (a, b) {return b.launchTime - a.launchTime});
+    var now = Date.now()
+    running.forEach(function (o) {
+      output({'timestamp':now, 'data':o, 'state':'running'});
+    })
+  })
+}
