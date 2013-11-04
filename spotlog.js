89a70b86b176dc00fed44161d51b6bc571170a6a
Taras Glek <taras@mozilla.com>
2013-11-04 14:10:46 -0800

    amazon refund math

--- spotlog.js
+++ spotlog.js
@@ -1,8 +1,8 @@
 const FIREBASE = 'https://ec2-dev-activity.firebaseio.com/'
 var firebase = new Firebase(FIREBASE);
 
-function age(timestamp) {
-  var diff_s = (new Date() - timestamp)/1000
+function human_interval(diff_ms) {
+  var diff_s = diff_ms/1000
   if (diff_s < 60)
     return Math.round(diff_s) + " seconds"
   var diff_m = diff_s / 60;
@@ -15,7 +15,20 @@ function age(timestamp) {
   return Math.round(diff_d) + " days"
 }
 
-function calcSpotCost(launchTime, endTime, prices) {
+
+function time_ago(timestamp) {
+  return human_interval(new Date() - timestamp)
+}
+
+function ms_to_hours(diff) {
+  return diff/1000/60/60;
+}
+
+function hours_to_ms(hours) {
+  return hours * 60 * 60 * 1000;
+}
+
+function calcSpotCost(launchTime, endTime, prices, roundHoursUp) {
   var keys = Object.keys(prices)
   keys.sort(function(a, b) {return a - b})
   var firstTime = null;
@@ -28,26 +41,22 @@ function calcSpotCost(launchTime, endTime, prices) {
       lastTime = i
   }
 
-  function to_hours(diff) {
-    return diff/1000/60/60;
-  }
-
   function cost(launchTime, endTime, costBegin, costEnd) {
     var costStamp = keys[costBegin]
     var hourlyPrice = prices[costStamp];
 
     if (costBegin == costEnd)
-      return to_hours(endTime - launchTime) * hourlyPrice;
+      return ms_to_hours(endTime - launchTime) * hourlyPrice;
     
     var nextTime = Math.min(keys[costBegin + 1], endTime)
     // node started before we got pricing data..fake the beginning of data
 
     if (launchTime < costStamp) {
-      //console.log("estimate:"+launchTime + " to " + costStamp + "("+age(costStamp - launchTime)+")"+" = " + hourlyPrice)
-      return to_hours(costStamp - launchTime) * hourlyPrice + cost(costStamp, endTime, costBegin, costEnd);
+      //console.log("estimate:"+launchTime + " to " + costStamp + "("+time_ago(costStamp - launchTime)+")"+" = " + hourlyPrice)
+      return ms_to_hours(costStamp - launchTime) * hourlyPrice + cost(costStamp, endTime, costBegin, costEnd);
     }
-    //console.log(costStamp + " to " + nextTime + "("+age(nextTime - costStamp)+")"+" = " + hourlyPrice)
-    return to_hours(nextTime - launchTime) * hourlyPrice + cost(nextTime, endTime, costBegin + 1, costEnd);
+    //console.log(costStamp + " to " + nextTime + "("+time_ago(nextTime - costStamp)+")"+" = " + hourlyPrice)
+    return ms_to_hours(nextTime - launchTime) * hourlyPrice + cost(nextTime, endTime, costBegin + 1, costEnd);
   }
 
   return cost(launchTime, endTime, firstTime, lastTime);
@@ -58,22 +67,40 @@ function format_price(price) {
 }
 
 // fb.child("spot").child("us-west-2a").on("value", function(s) {alert(uneval(s.val()))})
-function add_cost(launchTime, endTime, instanceType, availabilityZone, jqNode) {
+function add_cost(launchTime, endTime, instanceType, availabilityZone, stateReason, jqNode) {
   var diff = endTime - launchTime;
   var instanceKey = instanceType.replace('.', '_')
   firebase.child('ondemand').child('us-west-2').child(instanceKey).on('value', function(snapshot) {
     var perHour = snapshot.val();
-    var hours = Math.ceil(diff/1000/60/60);
+    var hours = Math.ceil(ms_to_hours(diff));
     jqNode.append($("<span><b> $"+format_price(hours * perHour)+"</b> ("+hours+" hours)</span>"))
   })
   
   firebase.child("spot").child(availabilityZone).child(instanceKey).on('value', function(snapshot) {
     var prices = snapshot.val()
-    var price = calcSpotCost(launchTime, endTime, prices)
+    if (!prices)
+      return
+    var free_duration = 0;
+    var hours = ms_to_hours(endTime - launchTime);
+    if (stateReason == "Server.SpotInstanceTermination") {
+      var floor_hours = Math.floor(hours)
+      free_duration = hours_to_ms(hours - floor_hours);
+      hours = floor_hours;
+    } else {
+      hours = Math.ceil(hours);
+    }
+    var billingEndTime = launchTime + hours_to_ms(hours)
+    var price = calcSpotCost(launchTime, billingEndTime, prices)
+    
     jqNode.append("<span><b> s$" + format_price(price) + "</b></span>");
+    if (free_duration) {
+      price = calcSpotCost(billingEndTime,billingEndTime + free_duration, prices);
+      jqNode.append("<span> Amazon donated <b> s$" + price + "</b>("+human_interval(free_duration)+")</span>");  
+    }
+
   })
 }
-
+// 
 
 function output(l, show_age, price_filter) {
   // Server.SpotInstanceTermination stateReason
@@ -82,11 +109,14 @@ function output(l, show_age, price_filter) {
    return;
    }*/
   var str =  l.data.instanceId + "("+l.data.instanceType+") " + l.state + " " + l.data.keyName;
-  var ageStr = show_age ? "<i>" + age(l.timestamp) + "</i>: " : "";
+  var ageStr = show_age ? "<i>" + time_ago(l.timestamp) + "</i>: " : "";
   var node = $("<div>" + ageStr
                + str + "</div>");
   if (!price_filter || l.state.indexOf(price_filter)!=-1) {
-    add_cost(new Date(l.data.launchTime), l.timestamp, l.data.instanceType, l.data.placement.availabilityZone, node);
+    if (l.data.keyName == 'taras')
+      console.log(l.data)
+    add_cost(new Date(l.data.launchTime)*1, l.timestamp, l.data.instanceType,
+             l.data.placement.availabilityZone, l.data.stateReason.code, node);
   }
   //console.log(l)
   // trigger firefox apptab notification
