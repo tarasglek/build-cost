544448b4a9d675d9859f000fa6527c2a8aa6165f
Taras Glek <taras@mozilla.com>
2013-11-04 13:29:07 -0800

    got spot math sorta working

--- spotlog.js
+++ spotlog.js
@@ -1,5 +1,5 @@
 const FIREBASE = 'https://ec2-dev-activity.firebaseio.com/'
-var awsPrices = new Firebase(FIREBASE + 'ondemand/us-west-2');
+var firebase = new Firebase(FIREBASE);
 
 function age(timestamp) {
   var diff_s = (new Date() - timestamp)/1000
@@ -15,7 +15,64 @@ function age(timestamp) {
   return Math.round(diff_d) + " days"
 }
 
+function calcSpotCost(launchTime, endTime, prices) {
+  var keys = Object.keys(prices)
+  keys.sort(function(a, b) {return a - b})
+  var firstTime = null;
+  var lastTime = null;
+  for(var i = 0;i<keys.length;i++) {
+    var time = keys[i];
+    if (firstTime == null || launchTime > time)
+      lastTime = firstTime = i //might as well initialize last time here)
+    else if (endTime > time)
+      lastTime = i
+  }
+
+  function to_hours(diff) {
+    return diff/1000/60/60;
+  }
+
+  function cost(launchTime, endTime, costBegin, costEnd) {
+    var costStamp = keys[costBegin]
+    var hourlyPrice = prices[costStamp];
+
+    if (costBegin == costEnd)
+      return to_hours(endTime - launchTime) * hourlyPrice;
+    
+    var nextTime = Math.min(keys[costBegin + 1], endTime)
+    // node started before we got pricing data..fake the beginning of data
+
+    if (launchTime < costStamp) {
+      //console.log("estimate:"+launchTime + " to " + costStamp + "("+age(costStamp - launchTime)+")"+" = " + hourlyPrice)
+      return to_hours(costStamp - launchTime) * hourlyPrice + cost(costStamp, endTime, costBegin, costEnd);
+    }
+    //console.log(costStamp + " to " + nextTime + "("+age(nextTime - costStamp)+")"+" = " + hourlyPrice)
+    return to_hours(nextTime - launchTime) * hourlyPrice + cost(nextTime, endTime, costBegin + 1, costEnd);
+  }
+
+  return cost(launchTime, endTime, firstTime, lastTime);
+}
+
+// fb.child("spot").child("us-west-2a").on("value", function(s) {alert(uneval(s.val()))})
+function add_cost(launchTime, endTime, instanceType, availabilityZone, jqNode) {
+  var diff = endTime - launchTime;
+  var instanceKey = instanceType.replace('.', '_')
+  firebase.child('ondemand').child('us-west-2').child(instanceKey).on('value', function(snapshot) {
+    var perHour = snapshot.val();
+    var hours = Math.ceil(diff/1000/60/60);
+    jqNode.append($("<span><b> $"+(hours * perHour)+"</b> ("+hours+" hours)</span>"))
+  })
+  
+  firebase.child("spot").child(availabilityZone).child(instanceKey).on('value', function(snapshot) {
+    var prices = snapshot.val()
+    var price = calcSpotCost(launchTime, endTime, prices)
+    jqNode.append("<span><b> s$" + Math.round(price*100)/100 + "</b></span>");
+  })
+}
+
+
 function output(l, show_age, price_filter) {
+  // Server.SpotInstanceTermination stateReason
   /*if (!l.timestamp) {
    snapshot.ref().remove();
    return;
@@ -25,15 +82,10 @@ function output(l, show_age, price_filter) {
   var node = $("<div>" + ageStr
                + str + "</div>");
   if (!price_filter || l.state.indexOf(price_filter)!=-1) {
-    var diff = l.timestamp - new Date(l.data.launchTime)
-    var type = l.data.instanceType
-    awsPrices.child(type.replace('.', '_')).on('value', function(snapshot) {
-      var perHour = snapshot.val();
-      var hours = Math.ceil(diff/1000/60/60);
-      node.append($("<span><b> $"+(hours * perHour)+"</b> ("+hours+" hours)</span>"))
-    })
+    add_cost(new Date(l.data.launchTime), l.timestamp, l.data.instanceType, l.data.placement.availabilityZone, node);
   }
-  //  console.log(l)
+  //console.log(l)
+  // trigger firefox apptab notification
   document.title = l.state + " " + l.data.instanceId;
   $( ".container" ).prepend( node );
 }
